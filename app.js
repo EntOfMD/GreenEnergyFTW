f06f83dae02724d8b4c05f9f86c083aea73e2264
Ashell Fernando <ashellz2010@gmail.com>
2019-05-03 20:18:29 -0400

    added new npm pkgs

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
-const createError = require('http-errors');
 const express = require('express');
+const exphbs = require('express-handlebars');
 const path = require('path');
 const bodyParser = require('body-parser');
-const exphbs = require('express-handlebars');
+const createError = require('http-errors');
+const axios = require('axios');
+const cheerio = require('cheerio');
 
 const logger = require('morgan');
 
@@ -13,10 +15,10 @@ const htmlRouter = require('./routes/html');
 const app = express();
 
 app.engine(
-  'handlebars',
-  exphbs({
-    defaultLayout: 'main'
-  })
+    'handlebars',
+    exphbs({
+        defaultLayout: 'main'
+    })
 );
 app.set('view engine', 'handlebars');
 
@@ -30,18 +32,18 @@ app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
