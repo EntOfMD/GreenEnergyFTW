9f12f9aa10a76c3d9ebe3701e6e42a5b0094f930
Ashell Fernando <ashellz2010@gmail.com>
2019-05-05 18:03:58 -0400

    js file for jquery, somehow it doesn't work

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,45 @@
 $(function() {
     $('[data-toggle="tooltip"]').tooltip();
+    $('.rescrape').click(() => {
+        setTimeout(() => {
+            location.reload();
+        }, 3000);
+    });
+
+    $('#pullForm').click(() => {
+        $('#addCarouselCard').show('slow');
+    });
+
+    const $submit = $('#submit');
+    const poster = $('#poster'),
+        url = $('#img'),
+        title = $('#title');
+
+    const API = {
+        post: function(info) {
+            return $.ajax({
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                type: 'POST',
+                url: '/api/addCarousel',
+                data: info
+            });
+        }
+    };
+
+    const handleFormSubmit = event => {
+        event.preventDefault();
+
+        const cImg = {
+            poster: poster.val().trim(),
+            url: url.val().trim(),
+            title: title.val().trim()
+        };
+
+        if (!poster || !url || !title) {
+            alert('Make sure ALL the fields are filled! :)');
+            return;
+        }
+    };
 });
