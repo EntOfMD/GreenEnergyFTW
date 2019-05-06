d345467f27b96bb06138fad9102dd328f5ba2d5b
Ashell Fernando <ashellz2010@gmail.com>
2019-05-06 09:53:55 -0400

    disabled emails for the time being

--- routes/html.js
+++ routes/html.js
@@ -3,60 +3,61 @@ const { Article, Contact } = require('../models');
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    service: process.env.NODEMAILER_SERVICE,
-    auth: {
-        user: process.env.NODEMAILER_USER,
-        pass: process.env.NODEMAILER_PASS
-    }
+  service: process.env.NODEMAILER_SERVICE,
+  auth: {
+    user: process.env.NODEMAILER_USER,
+    pass: process.env.NODEMAILER_PASS
+  }
 });
 
 var mailOptions = {
-    from: process.env.NODEMAILER_FROM,
-    to: process.env.NODEMAILER_TO,
-    subject: `You got a new contact form filled!`,
-    html: ''
+  from: process.env.NODEMAILER_FROM,
+  to: process.env.NODEMAILER_TO,
+  subject: `You got a new contact form filled!`,
+  html: ''
 };
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('cover', {
-        siteTitle: 'GreenEnergyFTW - Your news for all things Green Energy'
-    });
+  res.render('cover', {
+    siteTitle: 'GreenEnergyFTW - Your news for all things Green Energy'
+  });
 });
 
 router.get('/app', (req, res) => {
-    Article.find({}, (err, doc) => {
-        if (err) throw err;
-        res.render('index', {
-            siteTitle: 'GreenEnergyFTW - Your news for all things Green Energy',
-            doc
-        });
+  Article.find({}, (err, doc) => {
+    if (err) throw err;
+    res.render('index', {
+      siteTitle: 'GreenEnergyFTW - Your news for all things Green Energy',
+      doc
     });
+  });
 });
 
 router
-    .route('/letsgrabacoffee')
-    .get((req, res) => {
-        res.render('contact', {
-            siteTitle: `Contact Me, Let's grab a coffee :)`
-        });
-    })
-    .post((req, res) => {
-        let body = req.body;
-        let firstName = body.firstName,
-            lastName = body.lastName,
-            email = body.email,
-            handle = body.socialHandle,
-            link = body.link,
-            msg = body.msg;
+  .route('/letsgrabacoffee')
+  .get((req, res) => {
+    res.render('contact', {
+      siteTitle: `Contact Me, Let's grab a coffee :)`
+    });
+  })
+  .post((req, res) => {
+    let body = req.body;
+    let firstName = body.firstName,
+      lastName = body.lastName,
+      email = body.email,
+      handle = body.socialHandle,
+      link = body.link,
+      msg = body.msg;
 
-        if (!firstName && !lastName && !email && !msg) {
-            res.render('error', {
-                message:
-                    'Either First Name, Last Name, Email, and/or Message was missing.'
-            });
-        } else {
-            Contact.create(body).then(result => {
+    if (!firstName && !lastName && !email && !msg) {
+      res.render('error', {
+        message:
+          'Either First Name, Last Name, Email, and/or Message was missing.'
+      });
+    } else {
+      Contact.create(body).then(result => {
+        /*  //email
                 mailOptions.html = `<h1>Hey Boss!</h1><br/>
                 ${result.firstName} ${
                     result.lastName
@@ -72,13 +73,13 @@ router
                 transporter.sendMail(mailOptions, (err, info) => {
                     if (err) throw err;
                     console.log(`Email Succesfully sent. ${info.response}`);
-                });
-                res.render('contactSuccess', {
-                    result,
-                    siteTitle: `Contact Me, Let's grab a coffee :)`
-                });
-            });
-        }
-    });
+                }); */
+        res.render('contactSuccess', {
+          result,
+          siteTitle: `Thanks ${result.firstName}, I'll be in touch! :)`
+        });
+      });
+    }
+  });
 
 module.exports = router;
